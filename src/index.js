5b428746dba2613769031618696b7c578b90321e
CatDev <rmy-aurore@protonmail.ch>
2021-04-07 16:10:53 +0200

    remove useless files

--- src/index.js
+++ src/index.js
@@ -5,6 +5,3 @@ import App from "./App";
 import "./index.scss";
 
 ReactDOM.render(<App />, document.getElementById("root"));
-
-// Only change code below this line
-console.log("Now I know React and Redux!");
