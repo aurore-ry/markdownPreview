4df0b36d8c367a97c7cc3ec9f42391c0edba4a8c
CatDev <rmy-aurore@protonmail.ch>
2021-04-12 12:09:18 +0200

    Revert "remove useless files"
    This reverts commit 5b428746dba2613769031618696b7c578b90321e.

--- src/index.js
+++ src/index.js
@@ -5,3 +5,6 @@ import App from "./App";
 import "./index.scss";
 
 ReactDOM.render(<App />, document.getElementById("root"));
+
+// Only change code below this line
+console.log("Now I know React and Redux!");
